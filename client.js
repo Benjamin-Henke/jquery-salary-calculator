b050d140c3b481c36dfe9b244783d0248faba216
Benjamin Henke <benjaminmhenke@gmail.com>
2021-07-22 22:44:00 -0500

    Created function to remove user input

--- client.js
+++ client.js
@@ -1,13 +1,41 @@
 $(document).ready(onReady);
 
 function onReady() {
-     $(document).on('click', $('submitButton'), submitButton);
+    $(document).on('click', '#submitButton', submitButton);
 
 } // end onReady
 
-
+let employee = [];
 
 // registers the Submit Button being pressed 
-function submitButton () {
+function submitButton() {
     console.log('Submit Button');
+
+    // run employeeInput
+    employeeInput();
 } // end submitButton
+
+// Need a function to take in employee data
+// push user input to a table or list?
+function employeeInput() {
+    // take in employee information
+    $('#firstNameInput').val();
+    $('#lastNameInput').val();
+    $('#idNumberInput').val();
+    $('#jobTitleInput').val();
+    $('#annualSalaryInput').val();
+    // push to employee array
+
+    // clear user input
+    clearUserInput();
+
+} // end employeeInput
+
+function clearUserInput (){
+    // empty inputs
+    $('#firstNameInput').val('');
+    $('#lastNameInput').val('');
+    $('#idNumberInput').val('');
+    $('#jobTitleInput').val('');
+    $('#annualSalaryInput').val('');
+  } // end clearUserInput
